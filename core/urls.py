c42232aeb28e8596e39413f81fcfe112977af63f
ebraham <ebraham@gmail.com>
2026-04-25 15:12:46 +0200

    حذف الملفات من المستودع

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include # أضف include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tasks.urls')), # أضف هذا السطر
-path('accounts/', include('django.contrib.auth.urls')),
-]
